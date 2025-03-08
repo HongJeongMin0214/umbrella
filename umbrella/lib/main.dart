250fe9317d026abe185dd661b64a55560c73b663
Doo_Moq <doomoq0215@gmail.com>
2025-03-09 06:34:00 +0900

    푸쉬테스트

--- umbrella/lib/main.dart
+++ umbrella/lib/main.dart
@@ -8,7 +8,9 @@ import 'screens/login_screen.dart';
 import 'screens/main_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
